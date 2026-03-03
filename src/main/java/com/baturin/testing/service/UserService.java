ece2b31f954773e2cc512ba1c7ad3cd73f6861ad
dmitry <dima-bat2003@mail.ru>
2026-03-03 23:11:23 +0300

    Test lifecycle

--- src/main/java/com/baturin/testing/service/UserService.java
+++ src/main/java/com/baturin/testing/service/UserService.java
@@ -2,12 +2,22 @@ package com.baturin.testing.service;
 
 import com.baturin.testing.dto.User;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class UserService {
 
+    private final List<User> users = new ArrayList<>();
+
     public List<User> getAll() {
-        return Collections.emptyList();
+
+        return users;
     }
+
+    public boolean add(User user) {
+        return  users.add(user);
+    }
+
+
 }
--- src/test/java/com/baturin/testing/service/UserServiceTest.java
+++ src/test/java/com/baturin/testing/service/UserServiceTest.java
@@ -1,15 +1,51 @@
 package com.baturin.testing.service;
 
-import org.junit.jupiter.api.Test;
+import com.baturin.testing.dto.User;
+import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS) // значит создастся один класс для всех тестов
+@TestInstance(TestInstance.Lifecycle.PER_METHOD) // по умолчанию, значит для каждого теста новый экземпляр класс
 class UserServiceTest {
+
+    private UserService userService;
+
+    @BeforeAll
+    static void init() {
+        System.out.println("Before all: ");
+    }
+
+    @BeforeEach
+    void prepare() {
+        System.out.println("Before each: " + this);
+        userService = new UserService();
+    }
 
     @Test
     void usersEmptyIfNoUserAdded() {
-        var userService = new UserService();
+        System.out.println("Test 1: " + this);
         var users = userService.getAll();
         assertTrue(users.isEmpty(), "User list must be empty");
+    }
+
+    @Test
+    void usersSizeIfUserAdded() {
+        System.out.println("Test 2: " + this);
+        userService.add(new User());
+        userService.add(new User());
+
+        var users = userService.getAll();
+        assertEquals(2, users.size(), "Users size is must be equal 2");
+    }
+
+    @AfterEach
+    void deleteDataFromDatabase() {
+        System.out.println("After each: " + this);
+    }
+
+    @AfterAll
+    static void close() {
+        System.out.println("After all: ");
     }
 }
